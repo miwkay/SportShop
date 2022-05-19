ceabc11b9a4e02f097ddcc3e475f42d5058631d8
Khlevnoy Michael <87184891+miwkay@users.noreply.github.com>
2022-05-19 16:03:45 +0300

    Delete sportinggoodsstore directory

--- sportinggoodsstore/dumbbells/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# from models.Fitness import Fitness
-# from models.SportNutrition import SportNutrition
-# from models.WeightLifting import WeightLifting
-#
-# admin.site.register(Fitness)
-# admin.site.register(SportNutrition)
-# admin.site.register(WeightLifting)
