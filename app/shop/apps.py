9c75c60f521a802dfc75c033a88259caeabbbf63
Miwkay <jedy3666@gmail.com>
2022-05-20 10:25:32 +0300

    fix migration issue

--- app/shop/apps.py
+++ app/shop/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ShopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.model.BigAutoField'
     name = 'shop'
